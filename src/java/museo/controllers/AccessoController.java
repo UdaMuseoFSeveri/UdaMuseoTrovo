cf6833835f805d860e8b5b16fcfcd61b6d9550f3
FSEVERI\benetti3004 <FSEVERI\benetti3004@lmatbi-04.fseveri.it>
2016-04-13 13:34:25 +0200

    Database.java controllo user e aggiunta user aggiunt getBiglietti() sistemati alcune variabili aggiunti include per head e menu controller

--- src/java/museo/controllers/AccessoController.java
+++ src/java/museo/controllers/AccessoController.java
@@ -13,22 +13,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-
 @Controller
 public class AccessoController {
+
     private Database db = new Database();
-    
-    @RequestMapping(value="/login",method=RequestMethod.GET)
-    public String loginPage(ModelMap map){
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage(ModelMap map) {
         return "login";
     }
-    
-        @RequestMapping(value="/verificaLogin",method=RequestMethod.POST)
-    public String verificaLogin(ModelMap map,@RequestParam(value="utente", required=true)String nomeUtente,@RequestParam(value="password", required=true)String password){
-        map.put("login",db.verificaUtente(new Utente(nomeUtente,password)));
-        return "verificaLogin";
+
+    @RequestMapping(value = "/verificaLogin", method = RequestMethod.POST)
+    public String verificaLogin(ModelMap map, @RequestParam(value = "utente", required = true) String nomeUtente, @RequestParam(value = "password", required = true) String password) {
+        int i;
+        i = db.verificaUtente(new Utente(nomeUtente, password));
+        if (i == 1) {
+            return "login?indb=false";
+        }
+        if (i == 0) {
+            return "/";
+        }
+        return "login?pswdErr=true";
+
     }
-    
-    
+
+    @RequestMapping(value = "/registra", method = RequestMethod.POST)
+    public String registra(ModelMap map, @RequestParam(value = "utente", required = true) String nomeUtente, @RequestParam(value = "password", required = true) String password, @RequestParam(value = "verificaPassword", required = true) String verificaPassword) {
+        if (0 == db.utenteEsistente(nomeUtente)) {
+            if (password.equals(verificaPassword)) {
+                db.salvaUtente(new Utente(nomeUtente, password));
+                return "homepage";
+            } else {
+                return "login?errorPass=true";
+            }
+        } else {
+            return "login?dupplicate=true";
+        }
+    }
 }
