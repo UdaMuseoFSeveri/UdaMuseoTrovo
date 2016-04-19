a671689ff680d6666423b56f6c2b368db0ed969b
FSEVERI\magro3026 <FSEVERI\magro3026@lmatbi-03.fseveri.it>
2016-04-19 08:31:57 +0200

    Controller

--- src/java/museo/controllers/VisiteController.java
+++ src/java/museo/controllers/VisiteController.java
@@ -13,10 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- *
- * @author FSEVERI\reginato2906
- */
+
 @Controller
 public class VisiteController {
     private Database db;
@@ -31,8 +28,17 @@ public class VisiteController {
         esposizioni=(ArrayList<Visita>) db.getVisiteFromDate(dInizio, dFine);
     }
     
-    @RequestMapping(value={"","/homepage","/",})
+    @RequestMapping(value="/visite")
     public String visiteMuseo(ModelMap map){
+        Date dInizio=new Date(2016,01,01);
+        Date dFine=new Date(2016,12,31);
+        map.put("visite",esposizioni); 
+        map.put("visiteBase",visite_base);
+        return "visite";
+    }
+    
+    @RequestMapping(value={"","/homepage","/",})
+    public String homeMuseo(ModelMap map){
         Date dInizio=new Date(2016,01,01);
         Date dFine=new Date(2016,12,31);
         map.put("visite",esposizioni); 
